fe50f9101e63d2c2b55709340fe0de56148c0b28
Saulo Arruda <sauloarruda@gmail.com>
2012-07-28 14:46:06 -0400

    Initial Commit

---
+++ MapPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapPlayer
+//
+//  Created by Saulo Arruda Coelho on 7/28/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MapPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapPlayer
+//
+//  Created by Saulo Arruda Coelho on 7/28/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
